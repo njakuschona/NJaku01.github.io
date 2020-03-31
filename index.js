458a38aee2cff72d47f6dc139c5c0a7c36ce8377
nick1 <n_jaku01@wwu.de>
2020-03-31 11:13:26 +0200

    text

--- index.js
+++ index.js
@@ -25,12 +25,11 @@ function renderPlaces(places) {
        let latitude = place.location.lat;
        let longitude = place.location.lng;
 
-       let model = document.createElement('a-entity');
+       let model = document.createElement('a-text');
        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-       model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-       model.setAttribute('rotation', '0 180 0');
-       model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '0.5 0.5 0.5');
+       model.setAttribute('value', 'Ich hab dich lieb');
+       model.setAttribute('lock-at', '[gps-camera]');
+       model.setAttribute('scale', '120 120 120');
 
        model.addEventListener('loaded', () => {
            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
