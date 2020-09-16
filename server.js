bddf2bb6b07f6b792c21585f26afe41082e727d1
NJaku01 <n_jaku01@wwu.de>
2020-09-16 13:31:06 +0200

    images folder

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use("/public", express.static(__dirname + '/public'))
 
+app.use("/images", express.static(__dirname + '/images'))
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
